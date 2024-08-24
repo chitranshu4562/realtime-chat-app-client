a92d565dd52919b39024d423760e8e9a5313b65f
Chitranshoo Prakash <chitranshoo.prakash@jarvis.consulting>
2024-08-24 20:57:26 +0530

    sign-up form

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -5,3 +5,7 @@ import {API_URL} from "./config.js";
 export const login = (loginData) => {
     return axios.post(API_URL + 'authentication/login', loginData);
 }
+
+export const signUp = (signupData) => {
+    return axios.post(API_URL + 'authentication/signup-user', signupData);
+}
--- src/pages/sign-up-page/SignUpPage.jsx
+++ src/pages/sign-up-page/SignUpPage.jsx
@@ -1,7 +1,94 @@
+import {useFormik} from "formik";
+import * as Yup from "yup";
+import {EMAIL_REGEX} from "../../utils/Constants.js";
+import classes from "../login-page/LoginPage.module.css";
+import {Button} from "@mui/material";
+import {hasEmptyStringOnly} from "../../utils/utilHelper.js";
+import {displayLoader, hideLoader} from "../../utils/loaderHelper.js";
+import {signUp} from "../../api/authApi.js";
+import {errorNotification, successNotification} from "../../utils/notificationHelper.js";
+import {storeAuthDataInLocalStorage} from "../../utils/authHelper.js";
+import {dispatch} from "../../store.js";
+import {storeAuthData} from "../../features/authDataSlice.js";
+import {useNavigate} from "react-router-dom";
+
 export default function SignUpPage() {
+    const navigate = useNavigate();
+    const handleSignUp = (values) => {
+        displayLoader();
+        signUp(values)
+            .then(result => {
+                hideLoader();
+                successNotification(result.data.message);
+                signupForm.handleReset();
+                const authData = {
+                    authToken: result.data.authToken,
+                    expirationTime: result.data.expirationTime,
+                    user: result.data.user
+                }
+                storeAuthDataInLocalStorage(authData);
+                dispatch(storeAuthData(authData));
+                navigate('/');
+            })
+            .catch(error => {
+                hideLoader();
+                errorNotification(error.response.data.message);
+            });
+    }
+
+    const signupForm = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required('*Required').min(5, 'Name must have at least five characters'),
+            email: Yup.string()
+                .required('*Required').matches(EMAIL_REGEX, 'Email is invalid'),
+            password: Yup.string()
+                .required('*Required').min(6, 'Password must have at least six characters')
+        }),
+        onSubmit: handleSignUp
+    });
     return (
         <>
-            <h1>Sign Up Page</h1>
+            <div className={`shadow ${classes.formContainer}`}>
+                <h3 className={`text-center`}>SignUp Form</h3>
+                <form onSubmit={signupForm.handleSubmit}>
+                    <div className={`form-group my-2`}>
+                        <label htmlFor={`name`}>Name</label>
+                        <input id={`name`} type={`text`} {...signupForm.getFieldProps('name')}
+                               className={`form-control`} autoFocus/>
+                        {(signupForm.touched.name && signupForm.errors.name) &&
+                            <span className="text-danger">{signupForm.errors.name}</span>}
+                    </div>
+
+                    <div className={`form-group my-2`}>
+                        <label htmlFor={`email`}>Email</label>
+                        <input id={`email`} type={`text`} {...signupForm.getFieldProps('email')}
+                               className={`form-control`}/>
+                        {(signupForm.touched.email && signupForm.errors.email) &&
+                            <span className="text-danger">{signupForm.errors.email}</span>}
+                    </div>
+
+                    <div className={`form-group my-2`}>
+                        <label htmlFor={`password`}>Password</label>
+                        <input id={`password`} type={`password`} {...signupForm.getFieldProps('password')}
+                               className={`form-control`}/>
+                        {(signupForm.touched.password && signupForm.errors.password) &&
+                            <span className="text-danger">{signupForm.errors.password}</span>}
+                    </div>
+
+                    <div className={`d-flex justify-content-center`}>
+                        <Button variant="contained" type="submit"
+                                disabled={!signupForm.isValid || hasEmptyStringOnly(signupForm.values)}>
+                            Sign Up
+                        </Button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
